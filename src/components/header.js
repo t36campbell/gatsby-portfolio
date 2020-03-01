febdfb0ec3af21340e380fa89ec2a674dbfe7851
Tyler Campbell <tylercampbell@tylersmcbookpro.home>
2020-03-01 15:08:30 -0500

    added signature svg

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `transparent`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -14,17 +14,40 @@ const Header = ({ siteTitle }) => (
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        display: `flex`,
+        justifyContent: `space-evenly`
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
+        <Link to="/" style={{
+            color: `black`,
             textDecoration: `none`,
           }}
-        >
-          {siteTitle}
+        > Home
+        </Link>
+      </h1>
+      <h1 style={{ margin: 0 }}>
+        <Link to="/" style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        > About
+        </Link>
+      </h1>
+      <h1 style={{ margin: 0 }}>
+        <Link to="/" style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        > Projects
+        </Link>
+      </h1>
+      <h1 style={{ margin: 0 }}>
+        <Link to="/" style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        > Contact
         </Link>
       </h1>
     </div>
