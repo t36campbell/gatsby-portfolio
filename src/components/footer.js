52cbbd90adb8efbae8a5a1c69066aed80fefde8b
Tyler Campbell <tylercampbell@tylersmcbookpro.home>
2020-03-07 22:49:59 -0500

    added styled div to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import '../utils/fontawesome'
 import ButtonLink from '../components/button_link'
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons'
@@ -8,28 +9,28 @@ const Footer = () => {
     const icon_size = "2x";
 
     return (
-    <footer style={{
-        background: `transparent`, 
-        position: `fixed`,
-        width: `12%`,
-        left: `3%`,
-        bottom: `2%`,         
-    }}
-    >
-        <div
-            style={{
-                display: `flex`,
-                justifyContent: `space-evenly`
-            }}
-        >
+    <FooterContainer>
+        <IconContainer>
             <ButtonLink to={''} icon={['fab', 'github-square']} color={icon_color} size={icon_size}/>    
             <br></br>
             <ButtonLink to={''} icon={faFilePdf} color={icon_color} size={icon_size}/>  
             <br></br>  
             <ButtonLink to={''} icon={['fab', 'linkedin']} color={icon_color} size={icon_size}/> 
-        </div>
-  </footer>
+        </IconContainer>
+    </FooterContainer>
     )
 }
 
 export default Footer
+
+const FooterContainer = styled.div`
+    background: transparent;
+    position: fixed;
+    width: 12%;
+    left: 3%;
+    bottom: 2%;   
+`
+const IconContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
