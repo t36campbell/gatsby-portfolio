daec878fad18c0335643581be847fde5a76e922f
t36campbell <campbell3636@yahoo.com>
2020-03-13 22:31:38 -0400

    fixed redering issue with navbar in safari

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -5,13 +5,12 @@ import styled from "@emotion/styled"
 import Footer from "./footer"
 
 const Sidenav = () => {
-  const link_styles = css({
-    textDecoration: "none",
-    color: "#c6c6c6",
-    fontFamily: "Playfair Display",
-    fontSize: "2rem",
-  })
   const NavContainer = styled.div`
+    height: 100%;
+    width: 15%;
+    overflow-x: hidden;
+  `
+  const Sidebar = styled.div`
     margin-bottom: 2rem;
     height: 100%;
     width: 3%;
@@ -20,26 +19,30 @@ const Sidenav = () => {
     top: 0;
     left: 0;
     background-color: #000;
-    overflow-x: hidden;
   `
   const NavLinks = styled.div`
-    margin: 0 auto;
-    max-width: 960;
     padding: 5rem 1.25rem;
     height: 100%;
     width: 12%;
     position: fixed;
     z-index: 1;
+    top: 0;
     left: 3%;
     background-color: #000;
     opacity: 0.81;
-    overflow-x: hidden;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
   `
+  const link_styles = css({
+    textDecoration: "none",
+    color: "#c6c6c6",
+    fontFamily: "Playfair Display",
+    fontSize: "2rem",
+  })
   return (
     <NavContainer>
+      <Sidebar/>
       <NavLinks>
         <Link to="/" css={link_styles}>
           Home
