da389b356b4b5a1bd2e3e24c11f2c93087a4a8f4
t36campbell <campbell3636@yahoo.com>
2020-03-13 20:53:33 -0400

    added styled container for page in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,22 +10,28 @@ import styled from "@emotion/styled"
 import Sidenav from "./sidenav"
 
 const Layout = ({ children }) => {  
+  
+  const PageContainer = styled .div`
+    height: 100vh;
+    width: 100vw;
+  `
+  const MainContainer = styled.div`
+    width: 70%;
+    margin-left: 20%;
+    margin-right: 20%;
+    padding: 5% 5%;
+    max-width: 70%;
+  `
+  
   return (
-    <>
+    <PageContainer>
       <Sidenav/>
       <MainContainer>
         <main>{children}</main>
       </MainContainer>
-    </>
+    </PageContainer>
   )
 }
 
 export default Layout
 
-const MainContainer = styled.div`
-  width: 70%;
-  margin-left: 20%;
-  margin-right: 20%;
-  padding: 5% 5%;
-  max-width: 70%;
-`
