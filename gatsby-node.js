51f7e5f123e31a3b4cf70f2e1a00edee4b8e788f
t36campbell <campbell3636@yahoo.com>
2020-03-20 23:15:47 -0400

    adjusted dates of posts to ensure proper sorting

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,12 @@ exports.createPages = ({ actions, graphql }) => {
 	const postTemplate = path.resolve('src/templates/blog-post.js');
 	const projectTemplate = path.resolve('src/templates/project-page.js');
 
-	// Individual blogs pages
+	// Individual post pages
 	const posts = graphql(`
 		{
 			posts: allMarkdownRemark(
 				filter: { fileAbsolutePath: { glob: "**/src/pages/posts/*.md" } }
-				sort: { fields: frontmatter___date, order: ASC }
+				sort: { fields: frontmatter___date, order: DESC }
 			) {
 				edges {
           node {
@@ -31,7 +31,7 @@ exports.createPages = ({ actions, graphql }) => {
 			Promise.reject(result.errors);
 		}
 
-		// Create blog pages
+		// Create post pages
 		result.data.posts.edges.forEach(({ node }) => {
 			createPage({
 				path: node.frontmatter.path,
@@ -40,14 +40,14 @@ exports.createPages = ({ actions, graphql }) => {
 		});
 	});
 
-	// Individual docs pages
+	// Individual projects pages
 	const projects = graphql(`
 		{
 			projects: allMarkdownRemark(
 				filter: {
 					fileAbsolutePath: { glob: "**/src/pages/projects/*.md" }
 				}
-				sort: { fields: frontmatter___date, order: ASC }
+				sort: { fields: frontmatter___date, order: DESC }
 			) {
 				edges {
           node {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,20 +2,38 @@ import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Link from 'gatsby-link'
+import styled from "@emotion/styled"
 
 export default function Template({ data }) {
   const post = data.markdownRemark
-
+  const PostCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #000;
+    opacity: 0.81;
+    margin: 1rem;
+    padding: 1rem;
+    font-size: 1rem;
+    color: #c6c6c6;
+    height: 100%;
+    width:  100%;
+    border-radius: 4px;
+    transition: all 500ms;
+    overflow: hidden;
+  `
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <Link to="/blog/">Go Back</Link>
-        <hr />
+        <PostCard>
         <h1>{post.frontmatter.title}</h1>
         <h4>
           Posted by {post.frontmatter.author} on {post.frontmatter.date}
         </h4>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Link to="/blog/">Go Back</Link>
+        </PostCard>
     </Layout>
   )
 }
