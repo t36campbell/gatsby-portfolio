8e561635568b64181f04209a11324c9812c3f69b
t36campbell <campbell3636@yahoo.com>
2020-03-30 19:49:57 -0400

    adjusted form and added bot-field

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,19 +21,60 @@ const ContactPage = () => {
         <h1>Contact Me</h1>
         <form css={flex_form} name='contact' method='POST' action='https://getform.io/f/779f19d9-dcb1-4a14-82ee-7004061fc1a2' netlify-honeypot="bot-field" data-netlify="true">
           <input type="hidden" name="bot-field" />
-          <label css={full_width}>
-            Name
-            <input type="text" name="name" />
+          <label for="firstName" css={full_width}>
+            <h3>First Name</h3>
           </label>
-          <label css={full_width}>
-            Email
-            <input type="email" name="email" />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            css={full_width}
+            placeholder="Enter your First Name"
+          ></input>
+          <label for="lastName">
+            <h3>Last Name</h3>
           </label>
-          <label css={full_width}>
-            Message
-            <input type="text" name="message" />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            css={full_width}
+            placeholder="Enter your Last Name"
+          ></input>
+          <label for="email">
+            <h3>Email</h3>
           </label>
-          <button type="submit">Send</button>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            css={full_width}
+            placeholder="Enter your Email"
+          ></input>
+          <label for="phone">
+            <h3>Phone</h3>
+          </label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            css={full_width}
+            placeholder="Enter your Phone #"
+          ></input>
+          <label for="message">
+            <h3>Reason</h3>
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="3"
+            css={full_width}
+            placeholder="Enter a brief description of the reason for your corespondence"
+          ></textarea>
+          <br></br>
+          <button type="submit" value="submit" css={full_width}>
+            Submit
+          </button>
         </form>
       </Container>
     </Layout>
