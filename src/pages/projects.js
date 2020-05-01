74ca4f8531532a1f3ec56f3e786d8a7f8173046b
t36campbell <campbell3636@yahoo.com>
2020-05-01 13:31:38 -0400

    updated project page w/ hover state and new query

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState }  from "react"
 import Layout from "../components/layout/index"
 import SEO from "../components/seo/index"
 import Card from "../components/card/index"
+import CardBody from "../components/card_body/index"
+import CardImg from "../components/card_img/index"
 import FeatureCard from "../components/feature_card/index"
 import Link from "gatsby-link"
 import styled from "@emotion/styled"
+import { css } from "@emotion/core"
 
 const ProjectPage = ({ data }) => {
   const Title = styled.h1`
@@ -19,6 +22,22 @@ const ProjectPage = ({ data }) => {
       grid-template-columns: repeat(auto-fill, 100%);
     }
   `
+  const underline = css({
+    textDecoration: "underline"
+  })
+  const featureTitle = css({
+    ":hover": underline,
+    textDecoration: "none",
+    color: "#ccc"
+  })
+  const postTitle = css({
+    ":hover": underline,
+    textDecoration: "none",
+    color: "#191919"
+  })
+
+  const [hover, setHover] = useState(false)
+
   return (
     <Layout>
       <SEO title="Projects" />
@@ -31,13 +50,25 @@ const ProjectPage = ({ data }) => {
               post.node.frontmatter.featured === "true"
           )
           .map(post => (
-            <FeatureCard key={post.node.id}>
-              <h1>{post.node.frontmatter.title}</h1>
-              <p>
-                Posted by {post.node.frontmatter.author} on{" "}
-                {post.node.frontmatter.published}
-              </p>
-              <Link to={post.node.frontmatter.path}>Read More</Link>
+            <FeatureCard 
+              key={post.node.id}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              <CardImg hover={hover}></CardImg>
+              <CardBody hover={hover}>
+                <Link 
+                  css={featureTitle} 
+                  activeStyle={{ textDecoration: "underline" }}
+                  to={post.node.frontmatter.path}
+                > 
+                  <h1>{post.node.frontmatter.title}</h1>
+                </Link>
+                <p>
+                  Posted by {post.node.frontmatter.author} on{" "}
+                  {post.node.frontmatter.published}
+                </p>
+              </CardBody>
             </FeatureCard>
           ))}
       </ProjectContainer>
@@ -47,15 +78,31 @@ const ProjectPage = ({ data }) => {
       <Title>Latest Projects</Title>
       <ProjectContainer>
         {data.allMarkdownRemark.edges
-          .filter(post => post.node.frontmatter.category === "project")
+          .filter(
+            post =>
+              post.node.frontmatter.category === "project" &&
+              post.node.frontmatter.featured === "false"
+          )
           .map(post => (
-            <Card key={post.node.id}>
-              <h1>{post.node.frontmatter.title}</h1>
-              <p>
-                Posted by {post.node.frontmatter.author} on{" "}
-                {post.node.frontmatter.published}
-              </p>
-              <Link to={post.node.frontmatter.path}>Read More</Link>
+            <Card 
+            key={post.node.id}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            >
+              <CardImg hover={hover}></CardImg>
+              <CardBody hover={hover}>
+                <Link 
+                  css={postTitle} 
+                  activeStyle={{ textDecoration: "underline" }}
+                  to={post.node.frontmatter.path}
+                > 
+                  <h1>{post.node.frontmatter.title}</h1>
+                </Link>
+                <p>
+                  Posted by {post.node.frontmatter.author} on{" "}
+                  {post.node.frontmatter.published}
+                </p>
+              </CardBody>
             </Card>
           ))}
       </ProjectContainer>
