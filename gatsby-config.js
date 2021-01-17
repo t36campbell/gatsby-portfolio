7a0f6ac6fdea457162c7a04183ae0ad0da25621d
t36campbell <campbell3636@yahoo.com>
2021-01-17 14:43:29 -0500

    trying to fix confusion with linters

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `T.S.C. Portfolio`,
-    description: `The portfolio of Tyler Campbell; Full Stack Software Engineer. United States Air Force veteran with a B.Sc. in Information Technology 
-                  specializing in Web Application Development.`,
-    author: `Tyler Campbell`,
-    siteUrl: `https://tylercampbell.space`,
+    title: 'T.S.C. Portfolio',
+    description: 'The portfolio of Tyler Campbell; Full Stack Software Engineer. United States Air Force veteran with a B.Sc. in Information Technology specializing in Web Application Development.',
+    author: 'Tyler Campbell',
+    siteUrl: 'https://tylercampbell.space',
   },
   plugins: [
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
@@ -18,16 +17,16 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
+        name: 'pages',
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-158530632-1",
+        trackingId: 'UA-158530632-1',
       },
     },
     {
@@ -35,23 +34,23 @@ module.exports = {
       options: {
         host: 'https://tylercampbell.space',
         sitemap: 'https://tylercampbell.space/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
+        policy: [{ userAgent: '*', allow: '/' }],
       }
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#ff2400`,
-        theme_color: `#fff`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#ff2400',
+        theme_color: '#fff',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 }
