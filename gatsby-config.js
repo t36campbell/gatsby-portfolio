5e57e79f84006fccd31efb538da9cc5724105503
t36campbell <campbell3636@yahoo.com>
2022-03-06 21:07:41 -0500

    added google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Atkinson Hyperlegible\:400,400i,700, 700i`
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
