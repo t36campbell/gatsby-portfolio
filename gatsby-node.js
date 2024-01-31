01b7325f2b60ed933cc3aa82f8e7991889cc1bc9
t36campbell <campbell3636@yahoo.com>
2024-01-30 22:43:09 -0500

    clean up + default props

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,43 @@
 const path = require('path');
 const { createRemoteFileNode } = require('gatsby-source-filesystem');
 
+const image = `
+image {
+  childImageSharp {
+    gatsbyImageData(
+      aspectRatio: 1.6 # 16:10
+      width: 1800
+    )
+  }
+}`;
+
+const edges = `
+edges {
+  node {
+    html
+    frontmatter {
+      ...fm
+    }
+    image {
+      childImageSharp {
+        gatsbyImageData(
+          aspectRatio: 1.6 # 16:10
+          width: 1800
+        )
+      }
+    }
+  }
+}`;
+
+const postQueries = (names) =>
+  names.map((name) => {
+    const filter = `filter: { fileAbsolutePath: { glob: "**/src/pages/${name}/**/*.md" } }`;
+    return `
+      ${name}: allMarkdownRemark(${filter}) {
+        ${edges}
+      }`;
+  });
+
 const query = `
 {
   lists: allMarkdownRemark(
@@ -12,113 +49,82 @@ const query = `
           path
           categories
         }
-        image {
-          childImageSharp {
-            gatsbyImageData(
-              aspectRatio: 1.6 # 16:10
-              width: 1800
-            )
-          }
-        }
+        ${image}
       }
     }
   }
-  posts: allMarkdownRemark(
-    filter: { fileAbsolutePath: { glob: "**/src/pages/posts/*.md" } }
-  ) {
-    edges {
-      node {
-        html
-        frontmatter {
-          path
-          category
-          date
-          published
-          title
-          author
-          description
-          image
-        }
-        image {
-          childImageSharp {
-            gatsbyImageData(
-              aspectRatio: 1.6 # 16:10
-              width: 1800
-            )
-          }
-        }
-      }
-    }
-  }
-  projects: allMarkdownRemark(
-    filter: { fileAbsolutePath: { glob: "**/src/pages/projects/*.md" } }
-  ) {
-    edges {
-      node {
-        html
-        frontmatter {
-          path
-          category
-          date
-          published
-          title
-          author
-          description
-          link
-          repo
-          image
-        }
-        image {
-          childImageSharp {
-            gatsbyImageData(
-              aspectRatio: 1.6 # 16:10
-              width: 1800
-            )
-          }
-        }
-      }
-    }
-  }
-  shop: allMarkdownRemark(
-    filter: { fileAbsolutePath: { glob: "**/src/pages/shop/*.md" } }
-  ) {
-    edges {
-      node {
-        html
-        frontmatter {
-          path
-          category
-          date
-          published
-          title
-          author
-          description
-          link
-          repo
-          image
-        }
-        image {
-          childImageSharp {
-            gatsbyImageData(
-              aspectRatio: 1.6 # 16:10
-              width: 1800
-            )
-          }
-        }
-      }
-    }
-  }
+  ${postQueries(['posts', 'projects', 'shop'])}
 }
+
+fragment fm on Frontmatter {
+  category
+  description
+  image
+  path
+  title
+}
+
 `;
 
 exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions;
+  const { createFieldExtension, createTypes } = actions;
 
-  createTypes(`
-    type MarkdownRemark implements Node {
+  createFieldExtension({
+    name: 'default',
+    extend(_options, _prevFieldConfig) {
+      return {
+        resolve(source) {
+          return source.author === null ? 'Tyler Campbell' : source.author;
+        },
+      };
+    },
+  });
+
+  createFieldExtension({
+    name: 'price',
+    extend(_options, _prevFieldConfig) {
+      return {
+        resolve(source) {
+          return source.price === 0 ? 'FREE' : source.price;
+        },
+      };
+    },
+  });
+
+  createFieldExtension({
+    name: 'published',
+    extend(_options, _prevFieldConfig) {
+      return {
+        resolve(source) {
+          const date = new Date(source.date);
+          const options = { year: 'numeric', month: 'short', day: 'numeric' };
+          return date.toLocaleDateString('en-us', options);
+        },
+      };
+    },
+  });
+
+  const definitions = [
+    `type MarkdownRemark implements Node {
       image: File @link(from: "fields.localFile")
-    }
-  `);
+      frontmatter: Frontmatter
+    }`,
+    `type Frontmatter {
+      author: String! @author
+      category: String!
+      date: String!
+      description: String
+      image: String
+      link: String
+      path: String!
+      price: String @price 
+      published: String @published
+      repo: String
+      title: String
+    }`,
+  ];
+
+  createTypes(definitions);
 };
 
 exports.onCreateNode = async ({
@@ -135,8 +141,10 @@ exports.onCreateNode = async ({
   };
 
   if (shouldCreateRemoteFileNode(node)) {
+    const imageDefault =
+      'https://ik.imagekit.io/t36campbell/Portfolio/blog-1_rbK4YAxj4.png';
     const fileNode = await createRemoteFileNode({
-      url: node.frontmatter.image,
+      url: node.frontmatter.image ?? imageDefault,
       parentNodeId: node.id,
       createNode,
       createNodeId,
